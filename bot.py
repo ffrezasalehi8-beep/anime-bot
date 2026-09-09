fc23ef86733d19daca7afff1f84c2993db45a6f9
ffrezasalehi8-beep <ffrezasalehi8@gmail.com>
2026-09-09 21:55:28 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -26,7 +26,8 @@ async def anime(update: Update, context: ContextTypes.DEFAULT_TYPE):
     try:
         url = f"https://api.jikan.moe/v4/anime?q={name}&limit=1"
         data = requests.get(url, timeout=15).json()
-
+        await update.message.reply_text(str(data))
+return
         if not data["data"]:
             await update.message.reply_text("پیدا نشد")
             return
